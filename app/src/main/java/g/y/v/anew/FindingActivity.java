97b620de8c8f5534a96960cccec40a7cd472d89d
galqayeva <galqayeva@gmail.com>
2017-10-23 18:09:28 +0400

    new

---
+++ app/src/main/java/g/y/v/anew/FindingActivity.java
@@ -0,0 +1,14 @@
+package g.y.v.anew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class FindingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finding);
+    }
+}
