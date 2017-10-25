bf8133a41c364637855f7b259d0de9d95bbf8e62
galqayeva <galqayeva@gmail.com>
2017-10-25 14:37:50 +0400

    nw

--- app/src/main/java/g/y/v/anew/MainActivity.java
+++ app/src/main/java/g/y/v/anew/MainActivity.java
@@ -11,61 +11,12 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Spinner spinner,spinner2;
-    Button btn;
-    ArrayAdapter adapter2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spinner = (Spinner) findViewById(R.id.spinner);
-        spinner2 = (Spinner) findViewById(R.id.spinner2);
-        btn=(Button)findViewById(R.id.button);
 
-        ArrayAdapter adapter=ArrayAdapter.createFromResource(this,R.array.category,android.R.layout.simple_spinner_dropdown_item);
-
-        spinner.setAdapter(adapter);
-        adapter2=ArrayAdapter.createFromResource(MainActivity.this,R.array.altcategoryA,android.R.layout.simple_spinner_dropdown_item);
-        spinner.setPrompt("Title");
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-
-                adapter2=ArrayAdapter.createFromResource(MainActivity.this,R.array.altcategoryB,android.R.layout.simple_spinner_dropdown_item);
-                btn.setText("bb");
-                spinner2.setAdapter(adapter2);
-
-                Toast.makeText(MainActivity.this, spinner.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
-                if(spinner.getSelectedItem().toString()=="Eylence"){
-                    adapter2=ArrayAdapter.createFromResource(MainActivity.this,R.array.altcategoryA,android.R.layout.simple_spinner_dropdown_item);
-                    btn.setText("aaa");
-                    spinner2.setAdapter(adapter2);
-
-                }
-                if(spinner.getSelectedItem().toString()=="Cafe")
-                {
-                    adapter2=ArrayAdapter.createFromResource(MainActivity.this,R.array.altcategoryB,android.R.layout.simple_spinner_dropdown_item);
-                    btn.setText("bb");
-                    spinner2.setAdapter(adapter2);
-                }
-                if(spinner.getSelectedItem().toString()=="Restaurant"){
-                    adapter2=ArrayAdapter.createFromResource(MainActivity.this,R.array.altcategoryC,android.R.layout.simple_spinner_dropdown_item);
-                    btn.setText("ca");
-                    spinner2.setAdapter(adapter2);
-                }
-
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-
-            }
-        });
     }
 }
