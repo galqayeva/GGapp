314cda52482ddba9a23219ef35aea6d240f34dee
galqayeva <galqayeva@gmail.com>
2017-10-25 15:14:58 +0400

    model created

--- app/src/main/java/g/y/v/anew/Models/Category.java
+++ app/src/main/java/g/y/v/anew/Models/Category.java
@@ -5,4 +5,185 @@ package g.y.v.anew.Models;
  */
 
 public class Category {
+
+    private String clothesText,cosmeticsText,entertainText,medicineText,restauranText,birthdayText,marketText,transportText;
+    private String tecnhologyText,jewelleryText,otherText;
+    private int clothesM,cosmeticsM,entertainM,medicineM,restauranM,birthdayM,marketTextM,transportTextM;
+    private int tecnhologyM,jewelleryM,otherM;
+
+    public String getClothesText() {
+        return clothesText;
+    }
+
+    public void setClothesText(String clothesText) {
+        this.clothesText = clothesText;
+    }
+
+    public String getCosmeticsText() {
+        return cosmeticsText;
+    }
+
+    public void setCosmeticsText(String cosmeticsText) {
+        this.cosmeticsText = cosmeticsText;
+    }
+
+    public String getEntertainText() {
+        return entertainText;
+    }
+
+    public void setEntertainText(String entertainText) {
+        this.entertainText = entertainText;
+    }
+
+    public String getMedicineText() {
+        return medicineText;
+    }
+
+    public void setMedicineText(String medicineText) {
+        this.medicineText = medicineText;
+    }
+
+    public String getRestauranText() {
+        return restauranText;
+    }
+
+    public void setRestauranText(String restauranText) {
+        this.restauranText = restauranText;
+    }
+
+    public String getBirthdayText() {
+        return birthdayText;
+    }
+
+    public void setBirthdayText(String birthdayText) {
+        this.birthdayText = birthdayText;
+    }
+
+    public String getMarketText() {
+        return marketText;
+    }
+
+    public void setMarketText(String marketText) {
+        this.marketText = marketText;
+    }
+
+    public String getTransportText() {
+        return transportText;
+    }
+
+    public void setTransportText(String transportText) {
+        this.transportText = transportText;
+    }
+
+    public String getTecnhologyText() {
+        return tecnhologyText;
+    }
+
+    public void setTecnhologyText(String tecnhologyText) {
+        this.tecnhologyText = tecnhologyText;
+    }
+
+    public String getJewelleryText() {
+        return jewelleryText;
+    }
+
+    public void setJewelleryText(String jewelleryText) {
+        this.jewelleryText = jewelleryText;
+    }
+
+    public String getOtherText() {
+        return otherText;
+    }
+
+    public void setOtherText(String otherText) {
+        this.otherText = otherText;
+    }
+
+    public int getClothesM() {
+        return clothesM;
+    }
+
+    public void setClothesM(int clothesM) {
+        this.clothesM = clothesM;
+    }
+
+    public int getCosmeticsM() {
+        return cosmeticsM;
+    }
+
+    public void setCosmeticsM(int cosmeticsM) {
+        this.cosmeticsM = cosmeticsM;
+    }
+
+    public int getEntertainM() {
+        return entertainM;
+    }
+
+    public void setEntertainM(int entertainM) {
+        this.entertainM = entertainM;
+    }
+
+    public int getMedicineM() {
+        return medicineM;
+    }
+
+    public void setMedicineM(int medicineM) {
+        this.medicineM = medicineM;
+    }
+
+    public int getRestauranM() {
+        return restauranM;
+    }
+
+    public void setRestauranM(int restauranM) {
+        this.restauranM = restauranM;
+    }
+
+    public int getBirthdayM() {
+        return birthdayM;
+    }
+
+    public void setBirthdayM(int birthdayM) {
+        this.birthdayM = birthdayM;
+    }
+
+    public int getMarketTextM() {
+        return marketTextM;
+    }
+
+    public void setMarketTextM(int marketTextM) {
+        this.marketTextM = marketTextM;
+    }
+
+    public int getTransportTextM() {
+        return transportTextM;
+    }
+
+    public void setTransportTextM(int transportTextM) {
+        this.transportTextM = transportTextM;
+    }
+
+    public int getTecnhologyM() {
+        return tecnhologyM;
+    }
+
+    public void setTecnhologyM(int tecnhologyM) {
+        this.tecnhologyM = tecnhologyM;
+    }
+
+    public int getJewelleryM() {
+        return jewelleryM;
+    }
+
+    public void setJewelleryM(int jewelleryM) {
+        this.jewelleryM = jewelleryM;
+    }
+
+    public int getOtherM() {
+        return otherM;
+    }
+
+    public void setOtherM(int otherM) {
+        this.otherM = otherM;
+    }
 }
