737a080ee248a6938c220e56b091991d00aeccfb
galqayeva <galqayeva@gmail.com>
2017-10-30 17:22:23 +0400

    piechart

--- app/src/main/java/g/y/v/anew/MainActivity.java
+++ app/src/main/java/g/y/v/anew/MainActivity.java
@@ -1,12 +1,23 @@
 package g.y.v.anew;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.Adapter adapter1;
     RecyclerView rV1;
     Button btn;
+    PieChart pieChart;
+
+    private float[] yData= {25f, 10f, 15f, 10f, 5f, 15f, 20f};
+    private String[] xData={ "","Jessica" , "Mohammad" , "Kelsey", "Sam", "Robert", "Ashley","Mitch"} ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,18 +50,84 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        rV1=(RecyclerView)findViewById(R.id.recycleview);
-        rV1.setHasFixedSize(true);
-        rV1.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-        modelList1=new ArrayList<>();
 
-        for(int i=1;i<=16;i++){
-            CategoryModel demo=new CategoryModel("cfor clothing jaod die 18 avqust","120$");
-            modelList1.add(demo);
+        pieChart=(PieChart)findViewById(R.id.pieChart);
+        pieChart.setRotationEnabled(true);
+        //pieChart.setUsePercentValues(true);
+        // pieChart.setHoleColor(Color.BLUE);
+        //pieChart.setCenterTextColor(Color.BLACK);
+        pieChart.setHoleRadius(25f);
+        pieChart.setTransparentCircleAlpha(0);
+        pieChart.setCenterText("$");
+        pieChart.setCenterTextSize(10);
+        // pieChart.setDrawEntryLabels(true);
+        //pieChart.setEntryLabelTextSize(20);
+
+        addDataSet();
+        pieChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, Highlight h) {
+
+                int pos1 = e.toString().indexOf("(sum): ");
+                String sales = e.toString().substring(pos1 + 7);
+
+                for(int i = 0; i < yData.length; i++){
+                    if(yData[i] == Float.parseFloat(sales)){
+                        pos1 = i;
+                        break;
+                    }
+                }
+                String employee = xData[pos1 + 1];
+                Toast.makeText(MainActivity.this, "You spend " +sales + "$ \n" + "on " + employee, Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
+
+
+    }
+
+    private void addDataSet() {
+
+
+        ArrayList<PieEntry> yEntrys = new ArrayList<>();
+        ArrayList<String> xEntrys = new ArrayList<>();
+
+        for(int i = 0; i < yData.length; i++){
+            yEntrys.add(new PieEntry(yData[i] , i));
         }
 
-        adapter1=new MyAdapter(modelList1,getApplicationContext());
-        rV1.setAdapter(adapter1);
+        for(int i = 0; i < xData.length; i++){
+            xEntrys.add(xData[i]);
+        }
+        PieDataSet pieDataSet = new PieDataSet(yEntrys, "Speding");
+        pieDataSet.setSliceSpace(2);
+        pieDataSet.setValueTextSize(12);
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        colors.add(Color.WHITE);
+        colors.add(Color.YELLOW);
+        colors.add(Color.BLUE);
+        colors.add(Color.LTGRAY);
+        colors.add(Color.CYAN);
+        colors.add(Color.MAGENTA);
+        colors.add(Color.GREEN);
+
+        pieDataSet.setColors(colors);
+
+        Legend legend = pieChart.getLegend();
+        legend.setForm(Legend.LegendForm.CIRCLE);
+        legend.setPosition(Legend.LegendPosition.BELOW_CHART_CENTER);
+
+        PieData pieData = new PieData(pieDataSet);
+        pieChart.setData(pieData);
+        pieChart.invalidate();
+
+
 
     }
 }
---
+++ app/src/main/java/g/y/v/anew/demi.java
@@ -0,0 +1,34 @@
+package g.y.v.anew;
+
+/**
+ * Created by telim2 on 30.10.2017.
+ */
+
+public class demi {
+}
+//<android.support.v7.widget.RecyclerView
+//        android:id="@+id/recycleview"
+//        android:layout_width="match_parent"
+//        android:layout_height="match_parent"
+//        android:background="@color/colorPrimary"
+//        android:layout_below="@+id/textView5"
+//        android:layout_alignParentLeft="true"
+//        android:layout_alignParentStart="true"
+//        android:layout_marginTop="180dp"
+//        android:layout_marginLeft="20dp"
+//        android:layout_marginRight="20dp"
+//        android:layout_marginBottom="20dp">
+//</android.support.v7.widget.RecyclerView>
+//
+//        rV1=(RecyclerView)findViewById(R.id.recycleview);
+//        rV1.setHasFixedSize(true);
+//        rV1.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+//        modelList1=new ArrayList<>();
+//
+//        for(int i=1;i<=16;i++){
+//            CategoryModel demo=new CategoryModel("cfor clothing jaod die 18 avqust","120$");
+//            modelList1.add(demo);
+//        }
+//
+//        adapter1=new MyAdapter(modelList1,getApplicationContext());
+//        rV1.setAdapter(adapter1);
